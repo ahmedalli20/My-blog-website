d3fe5ec01b1bb85d71d8c96b73e09f6d2fb54afa
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-05 15:04:10 +0300

    add link to the Navbar compone nt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
+        <nav>
             
-        </div>
-    );
-};
+            <div className='navbar'>
+            <h1>My Blog App</h1>
+                <Link to="/">Home</Link>
+                <Link to="/createBlogs">Create Blog</Link>
+            </div>
+        </nav>
+     );
+}
 
 export default Navbar;
