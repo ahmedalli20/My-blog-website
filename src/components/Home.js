055d3db9b976257f4994daf8fcd4b51186b274a0
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-07 15:38:56 +0300

    fetched the data from dbjson file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import BlogDetails from './BlogDetails';
+import BlogList from './BlogList';
+
+
+
 
 const Home = () => {
+  
+    const[blogs, setBlogs] = useState([]);
+
+    const fetcher = () =>{
+        fetch("http://localhost:3004/blogs")
+        .then((response) => response.json())
+        .then((data) => {
+            //  console.log(data)
+          setBlogs(data);
+            });
+    }
+
+    useEffect(() => {
+        fetcher()
+        
+        }, []);
+
+
     return (
         <div>
-            
+              <BlogList blogs={blogs} title="All Blogs"/>  
+
+
         </div>
+
+
     );
 };
 
