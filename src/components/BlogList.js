e5ad9157b3fa7e91be8a9c54bab803e8841a5a79
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-07 15:39:36 +0300

    add function to dispaly the blogs

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
 
-const BlogList = () => {
+
+
+const BlogList = ({blogs, title}) => {
+
+  const blogLists = blogs.map(blog => ( 
+
+    <div className='bloglists' key={blog.id}>
+        
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+        </Link>
+     
+    </div>
+))
+
     return (
-        <div>
-            
+        <div className='blogs'>
+           <h1>{title}</h1>
+           {blogLists}
+        
         </div>
+   
+        
     );
 };
 
