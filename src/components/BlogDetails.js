edb66fd55c297101360248f62700860242a3b6b0
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-07 21:27:51 +0300

    Add blog details component

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,38 @@
 import React from 'react';
+  import { useParams, useNavigate } from 'react-router-dom';
+ import BlogList from './BlogList';
 
-const BlogDetails = () => {
+
+const BlogDetails = ({blogs}) => {
+    console.log(blogs);
+
+        const { id } = useParams();
+         const navigate = useNavigate();
+
+
+        //  blogs.filter(blog => (
+//          return blog.id === id
+        // ))
+
+ 
+    const handleClick = () => {
+        fetch(`http://localhost:3004/blogs/${blogs.id}` ,{
+            method: 'DELETE'
+        }).then(() => {
+             navigate('/Home');
+        })
+    }
     return (
         <div>
-            
+
+        <h2>{blogs.title}</h2>
+        <p>Written by {blogs.author}</p>
+        <div>{blogs.body}</div>
+        <button onClick={handleClick}>Delete</button>   
+
         </div>
+
+        
     );
 };
 
