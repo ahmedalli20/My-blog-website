b585e282ba2d344f87edc0602ea33667e87e2ee4
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-07 15:37:44 +0300

    Add route and import the other components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
 import React from 'react';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+ import BlogDetails from './components/BlogDetails';
+import CreateBlogs from "./components/CreateBlogs";
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import Login from './components/Login';
+
+
 
 const App = () => {
+
+  
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar  />
+        <div>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/Login" component={ Login} />
+            <Route path="/CreateBlogs" component={CreateBlogs} />
+            <Route path="/blogs/:id/" component={BlogDetails}   />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
